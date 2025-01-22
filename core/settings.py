da0bce36ae8d686c611d394442cca85e548dc13c
behruz171 <behruzzo662@gmail.com>
2025-01-22 17:58:46 +0500

    nimadir

--- core/settings.py
+++ core/settings.py
@@ -146,9 +146,9 @@ swagger_settings = {
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles'
 ]
 
 # Media files
