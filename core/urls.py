38ffd58b937c708fb1909c26e624b46c398ff1f9
behruz171 <behruzzo662@gmail.com>
2025-01-24 22:13:19 +0500

    nimadir

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,6 +27,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\\.json|\\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
 ]
 
 if settings.DEBUG:
