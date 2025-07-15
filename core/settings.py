755aa7439125a49431ae01d302bb94d04b1534e4
behruz171 <behruzzo662@gmail.com>
2025-07-15 13:21:05 +0500

    nimadir

--- core/settings.py
+++ core/settings.py
@@ -76,6 +76,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://star-claud-web.vercel.app"
 ]
 ROOT_URLCONF = 'core.urls'
 
